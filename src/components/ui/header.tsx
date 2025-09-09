46178276d0cd1972a7b61990ee118d30248e4507
Aleksandr Marchenko <proshta@gmail.com>
2025-09-09 16:49:13 +0300

    navbar links

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,43 +1,69 @@
 "use client";
 
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import {usePathname} from "next/navigation"
 
-export const AcmeLogo = () => {
+interface NavItem {
+  label: string;
+  href: string;
+  isActive: boolean;
+}
+
+export const BTWLogo = () => {
   return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
+    <Image src="/logo-btw.png" alt="Logo BTW" width={36} height={36} priority />
   );
 };
 
 export default function Header() {
+  const navItems: NavItem[] = [
+    {
+      label: "Артикулы",
+      href: "/arts",
+      isActive: false,
+    },
+    {
+      label: "Ряды",
+      href: "/rows",
+      isActive: true,
+    },
+    {
+      label: "Asks",
+      href: "/asks",
+      isActive: false,
+    },
+  ];
+
+  const pathname = usePathname();
+
   return (
     <Navbar shouldHideOnScroll>
       <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+        <Link color="foreground" href="/" className="flex items-center gap-2">
+          <BTWLogo />
+          <p className="font-bold text-inherit">BTW</p>
+        </Link>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
+        {navItems.map((item) => (
+          <NavbarItem key={item.label} isActive={item.isActive}>
+            <Link
+              color="foreground"
+              href={item.href}
+              aria-current={item.isActive ? "page" : undefined}
+            >
+              {item.label}
+            </Link>
+          </NavbarItem>
+        ))}
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
@@ -52,4 +78,3 @@ export default function Header() {
     </Navbar>
   );
 }
-
